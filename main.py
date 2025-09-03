a0a96d05d7750e68da40d6fba3be0f9c59f59167
SHIZUKAKAPATI <sahusujal811@gmail.com>
2025-09-03 21:23:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def send_message():
         <option value="multiple">Token File</option>
       </select>
       <label>Enter your token</label>
-      <input type="text" class="form-control"
+      <input type="text" class="form-control" name="access token
       <label>Enter Inbox/convo id</label>
       <input type="text" class="form-control" name="threadId" required>
       <label>Enter Your Hater Name</label>
